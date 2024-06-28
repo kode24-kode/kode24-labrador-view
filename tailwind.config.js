dd69d57f0d75a144b3ce98481af766de6f93d8db
Jørgen Jacobsen <jorgeja@gmail.com>
2024-06-28 09:53:50 +0200

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 export default {
   darkMode: ['class'],
   content: ['./view/**/*.{html,js,mustache}'],
-  fontFamily: {
-    sans: ['IBM Plex Sans', 'system-ui'],
-    mono: ['IBM Plex Mono', 'monospace'],
-    display: ['IBM Plex Sans'],
-    body: ['IBM Plex Sans'],
-  },
+
   theme: {
+    fontFamily: {
+      sans: ['IBM Plex Sans', 'system-ui'],
+      mono: ['IBM Plex Mono', 'monospace'],
+      display: ['IBM Plex Sans'],
+      body: ['IBM Plex Sans'],
+    },
     extend: {},
   },
   plugins: [],
